e3260010cce4370e48f352fb13f8d91dc6f05f8e
Miguel Torres <mtkphs16@gmail.com>
2021-04-18 21:24:15 -0500

    Added Cookie Banner

--- js/main.js
+++ js/main.js
@@ -31,3 +31,18 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+//Cookie Banner methods
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
+});
+
+setTimeout(() => {
+  if (!localStorage.getItem("cookieBannerDisplayed")) {
+    cookieContainer.classList.add("active");
+  }
+}, 3000);
